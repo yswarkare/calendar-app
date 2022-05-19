8b8587492ce84e87bab1eb8b2b12f3d4612b7a26
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-05-19 13:20:09 +0530

    added initial holidays

---
+++ src/data/holidays.data.js
@@ -0,0 +1,20 @@
+const holidays = [
+  {
+    title: 'Big Meeting',
+    allDay: true,
+    start: new Date(2022, 5, 0),
+    end: new Date(2022, 5, 0),
+  },
+  {
+    title: 'Vacation',
+    start: new Date(2022, 5, 7),
+    end: new Date(2022, 5, 10),
+  },
+  {
+    title: 'Conference',
+    start: new Date(2022, 5, 20),
+    end: new Date(2022, 5, 23),
+  },
+]
+
+export default holidays;
