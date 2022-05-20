0e7986a1107c5c85ea8983d583519375a835d009
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-05-20 19:47:35 +0530

    added button to Add New Holiday

--- src/components/AddHoliday/AddHoliday.js
+++ src/components/AddHoliday/AddHoliday.js
@@ -3,42 +3,50 @@ import { useContextState, useUpdateContextState } from '../../hooks/ContextState
 import DatePicker from 'react-datepicker';
 
 const AddHoliday = () => {
+	const [showInput, setShowInput] = useState(false);
 	const [newHoliday, setNewHoliday] = useState({ title: '', start: '', end: '' });
 	const allHolidays = useContextState().holidays;
 	const updateContextState = useUpdateContextState();
 
 	const handleAddHoliday = () => {
 		updateContextState('holidays', [...allHolidays, newHoliday]);
+		setShowInput(false);
 	};
 
 	return (
-		<div className={`w-full flex flex-col justify-center content-center items-center`}>
+		<div
+			className={`w-full px-8 py-8 gap-4 flex flex-col justify-center content-center items-center`}
+		>
 			<h2>Add New Holiday</h2>
-			<div className={`w-full flex flex-row justify-center content-center items-center`}>
-				<input
-					type='text'
-					placeholder='Add Title'
-					style={{ width: '20%', marginRight: '10px' }}
-					value={newHoliday.title}
-					onChange={(e) => setNewHoliday({ ...newHoliday, title: e.target.value })}
-				/>
-				<DatePicker
-					placeholderText='Start Date'
-					style={{ marginRight: '10px' }}
-					selected={newHoliday.start}
-					onChange={(start) => setNewHoliday({ ...newHoliday, start })}
-				/>
-				<DatePicker
-					placeholderText='End Date'
-					selected={newHoliday.end}
-					onChange={(end) => setNewHoliday({ ...newHoliday, end })}
-				/>
-			</div>
-			<div>
-				<button stlye={{ marginTop: '10px' }} onClick={() => handleAddHoliday()}>
-					Add Holiday
+			{!showInput && (
+				<button className={`btn-blue w-fit`} onClick={() => setShowInput(true)}>
+					Add
 				</button>
-			</div>
+			)}
+			{showInput && (
+				<div className={`w-full gap-4 flex flex-col justify-center content-center items-center`}>
+					<div className={`w-1/2 gap-4 flex flex-row justify-center content-center items-center`}>
+						<input
+							className={`w-full border-1 rounded`}
+							type='text'
+							placeholder='Add Title'
+							value={newHoliday.title}
+							onChange={(e) => setNewHoliday({ ...newHoliday, title: e.target.value })}
+						/>
+						<DatePicker
+							className={`w-full border-1 rounded`}
+							placeholderText='Date'
+							selected={newHoliday.start}
+							onChange={(date) => setNewHoliday({ ...newHoliday, start: date, end: date })}
+						/>
+					</div>
+					<div>
+						<button className={`btn-blue w-full`} onClick={() => handleAddHoliday()}>
+							Add Holiday
+						</button>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
