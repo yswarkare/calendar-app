e640432b993261650c0ca94813f8e895e30e49f3
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-05-20 19:45:29 +0530

    gave size to root component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-full min-h-[99vh] bg-[#fafafa]">
       <Home />
     </div>
   );
